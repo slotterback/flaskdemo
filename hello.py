1ffff3ec6525bb7ce900f768c6ef11e44ffd263f
slotterback <steve.slotterback@gmail.com>
2015-10-12 22:37:47 -0400

    URL Building

--- hello.py
+++ hello.py
@@ -1,31 +1,28 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask( __name__ )
 
 @app.route( '/' )
-def index():
-    return 'Index Page'
-    
-@app.route( '/hello' )
-def hello():
-    return 'Hello World'
+def index(): pass
 
+@app.route( '/login' )
+def login(): pass
+    
 @app.route( '/user/<username>' )
-def show_user_profile( username ):
-    # show the user profile for that user
-    return 'User %s' % username
-    
-@app.route( '/post/<int:post_id>' )
-def show_post( post_id ):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
+def profile( username ): pass
+        
+with app.test_request_context():
+    print( url_for( 'index' ) )
+    print( url_for( 'login' ) )
+    print( url_for( 'login', next = '/' ) )
+    print( url_for( 'profile', username = 'John Doe' ) )
+    '''
+    results are as follows:
+    /
+    /login
+    /login?next=/
+    /user/John%20Doe
+    '''
 
-@app.route( '/projects/' )# trailing slash => directory
-def projects():
-    return 'The project page'
-
-@app.route( '/about' )
-def about():
-    return 'The about page'
     
 if __name__ == '__main__':
     app.run( debug = True )
