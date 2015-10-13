d4f8a21d69b57fc331719209620e3dc32b1b0dbd
slotterback <steve.slotterback@gmail.com>
2015-10-12 22:25:05 -0400

    Unique URLs / Redirection Behavior

--- hello.py
+++ hello.py
@@ -19,5 +19,13 @@ def show_post( post_id ):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
 
+@app.route( '/projects/' )# trailing slash => directory
+def projects():
+    return 'The project page'
+
+@app.route( '/about' )
+def about():
+    return 'The about page'
+    
 if __name__ == '__main__':
     app.run( debug = True )
