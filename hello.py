10b34b383e69f677cb72b8eff6143af5087e4b3a
slotterback <steve.slotterback@gmail.com>
2015-10-13 21:15:21 -0400

    Accessing Request Data: Context Locals

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, Markup, request
 app = Flask( __name__ )
 
 @app.route( '/' )
@@ -35,6 +35,20 @@ def hello( name = None ):
     return render_template( 'hello.html', name = name )
     # searches /templates/ for 'hello.html'
     # alternatively, /application/templates/hello.html
+
+#test Markup class    
+Markup( '<strong>Hello %s!</strong>' ) % '<blink>hacker</blink>'
+Markup.escape('<blink>hacker</blink>')
+Markup('<em>Marked up</em> &raquo; HTML').striptags()
+
+with app.test_request_context( '/hello', method = 'POST' ):
+    # now you can do something with the request until the
+    # end of the with block, such as basic assertion
+    assert request.path == '/hello'
+    assert request.method == 'POST'
+    
+with app.request_context( environ ):
+    assert request.method = 'POST'
 
 if __name__ == '__main__':
     app.run( debug = True )
