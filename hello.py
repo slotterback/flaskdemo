7cb8a09da474b331abbc891501647c11bc2d9ad3
slotterback <steve.slotterback@gmail.com>
2015-10-13 20:04:48 -0400

    HTTP Methods

--- hello.py
+++ hello.py
@@ -4,8 +4,12 @@ app = Flask( __name__ )
 @app.route( '/' )
 def index(): pass
 
-@app.route( '/login' )
-def login(): pass
+@app.route( '/login', methods = [ 'GET', 'POST' ] )
+def login(): 
+    if request.method == 'POST':
+        do_the_login()
+    else:
+        show_the_login_form()
     
 @app.route( '/user/<username>' )
 def profile( username ): pass
